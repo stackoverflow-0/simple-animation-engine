07d53bf90c0ccdc64559ec7e6474045792270627
czh <3200100515@zju.edu.cn>
2023-09-21 13:12:53 +0800

    cc: fix cmake, add glew lib

--- cmake/FindGLEW.cmake
+++ cmake/FindGLEW.cmake
@@ -1,33 +1,26 @@
-set( _glew_HEADER_SEARCH_DIRS
-"/usr/include"
-"/usr/local/include"
-"${CMAKE_SOURCE_DIR}/include"
-"C:/Program Files (x86)/glew/include" )
-set( _glew_LIB_SEARCH_DIRS
-"/usr/lib"
-"/usr/local/lib"
-"${CMAKE_SOURCE_DIR}/lib"
-"C:/Program Files (x86)/glew/lib-msvc110" )
-
-# Check environment for root search directory
-set( _glew_ENV_ROOT $ENV{GLEW_ROOT} )
-if( NOT GLEW_ROOT AND _glew_ENV_ROOT )
-	set(GLEW_ROOT ${_glew_ENV_ROOT} )
-endif()
-
-# Put user specified location at beginning of search
-if( GLEW_ROOT )
-	list( INSERT _glew_HEADER_SEARCH_DIRS 0 "${GLEW_ROOT}/include" )
-	list( INSERT _glew_LIB_SEARCH_DIRS 0 "${GLEW_ROOT}/lib" )
-endif()
-
-# Search for the header
-FIND_PATH(GLEW_INCLUDE_DIR "GL/glew.h"
-PATHS ${_glew_HEADER_SEARCH_DIRS} )
-
-# Search for the library
-FIND_LIBRARY(GLEW_LIBRARY NAMES glew32 glew
-PATHS ${_glew_LIB_SEARCH_DIRS} )
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GLEW DEFAULT_MSG
-GLEW_LIBRARY GLEW_INCLUDE_DIR)
+FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
+	/usr/include
+	/usr/local/include
+	/opt/local/include
+	"${CMAKE_SOURCE_DIR}/include"
+)
+FIND_LIBRARY( GLEW_LIBRARY glew32
+	/usr/lib64
+	/usr/lib
+	/usr/local/lib
+	/opt/local/lib
+	"${CMAKE_SOURCE_DIR}/lib"
+)
+IF(GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
+	SET( GLEW_FOUND TRUE )
+	SET( GLEW_LIBRARIES ${GLEW_LIBRARY} )
+ENDIF(GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
+IF(GLEW_FOUND)
+	IF(NOT GLEW_FIND_QUIETLY)
+	MESSAGE(STATUS "Found GLEW: ${GLEW_LIBRARY}")
+	ENDIF(NOT GLEW_FIND_QUIETLY)
+ELSE(GLEW_FOUND)
+	IF(GLEW_FIND_REQUIRED)
+	MESSAGE(FATAL_ERROR "Could not find libGLEW")
+	ENDIF(GLEW_FIND_REQUIRED)
+ENDIF(GLEW_FOUND)
