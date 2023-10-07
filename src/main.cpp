0d0c70ba168f1d1c23adc0037569e740b3cff9a5
MeoMeoCat <2834395095@qq.com>
2023-10-07 12:41:52 +0800

    cc: fix fps

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <thread>
 #include <format>
 
+#include <chrono>
+
 int main()
 {
     // std::cout << sizeof(glm::dualquat) / sizeof(float) << std::endl;
@@ -38,15 +40,15 @@ int main()
 
     gizmo_model.load_with_config("asset/gizmo_config.json");
 
-    
+
     // shader.compile();
 
     auto world_matrix      = glm::mat4(1.0f);
-    
+
     auto projection_matrix = glm::perspectiveFov(glm::radians(60.0f), float(1024), float(768), 0.1f, 10.0f);
 
     auto time{0.0f};
-    auto last_clock = clock();
+    auto last_clock = std::chrono::_V2::system_clock::time_point{};
     auto frame_id{0};
 
     auto weight_left_frame{1.0f};
@@ -57,6 +59,13 @@ int main()
     auto show_bone_gizmo{false};
 
     auto bone_gizmo_color = glm::vec4(0.0f, 1.0f, 0.7f, 1.0f);
+
+    auto delta_frame_time{0.0f};
+
+    auto fps{0};
+    auto final_fps{0};
+
+    auto fps_sec{0.0f};
 
     auto display = [&]()
     {
@@ -77,18 +86,31 @@ int main()
         gizmo_shader.apply();
         gizmo_shader.setUniformMatrix4fv("world", world_matrix);
         gizmo_shader.setUniformMatrix4fv("viewProj", projection_matrix * view_matrix);
+        auto current_clock = std::chrono::high_resolution_clock().now();
+        // std::time(& current_clock);
+        delta_frame_time = double(std::chrono::duration_cast<std::chrono::nanoseconds>(current_clock - last_clock).count()) * 1e-9;
 
+        last_clock = current_clock;
+
+        fps++;
+        fps_sec += delta_frame_time;
+        if (fps_sec >= 1.0f) {
+            fps_sec = 0.0f;
+            final_fps = fps;
+            fps = 0;
+        }
         if (! human_with_skeleton.import_animation) {
+
             shader.apply();
             human_with_skeleton.draw();
             return;
         }
 
         if (human_with_skeleton.speed > 0.0f) {
-            time += float(clock() - last_clock) / float(CLOCKS_PER_SEC);
+            time += delta_frame_time;
             speed = human_with_skeleton.speed;
         }
-        last_clock = clock();
+
 
         auto& track = human_with_skeleton.tracks[human_with_skeleton.play_anim_track];
 
@@ -104,7 +126,7 @@ int main()
         shader.setUniform1f("right_weight", weight_right_frame);
         shader.setUniform1i("bone_current_pose", 2 + human_with_skeleton.play_anim_track);
         shader.setUniform1i("show_bone_weight_id", human_with_skeleton.show_bone_weight_id);
-        
+
         gizmo_shader.apply();
         gizmo_shader.setUniform1i("frame_id", frame_id);
         gizmo_shader.setUniform1f("left_weight", weight_left_frame);
@@ -112,16 +134,16 @@ int main()
         gizmo_shader.setUniform1i("bone_current_pose", 2 + human_with_skeleton.play_anim_track);
         gizmo_shader.setUniform4fv("gizmo_color", bone_gizmo_color);
         gizmo_shader.setUniform1f("gizmo_scale", gizmo_model.scale);
-        
+
 
         if (frame_id > track.duration - 1) {
             frame_id = 0;
         }
-    
+
         shader.apply();
         human_with_skeleton.draw();
 
-        if (show_bone_gizmo) {
+        if (show_bone_gizmo && gizmo_model.scale > 0.0f) {
             glDisable(GL_DEPTH_TEST);
             glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             glEnable(GL_BLEND);
@@ -138,7 +160,8 @@ int main()
         if (time >= frame_time) {
             frame_id++;
             time = 0.0f;
-            last_clock = clock();
+            // last_clock = ;
+            // fps = 0;
         }
     };
 
@@ -150,7 +173,7 @@ int main()
         glCullFace(GL_BACK);
         auto human_with_skeleton_config_scale = human_with_skeleton.scale;
         auto gizmo_model_config_scale = gizmo_model.scale;
-        
+
         do
         {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -159,6 +182,7 @@ int main()
                 ImGui::NewFrame();
                 auto anim_tool_active{true};
                 ImGui::Begin("Animation Tools", &anim_tool_active, ImGuiWindowFlags_::ImGuiWindowFlags_MenuBar);
+                ImGui::TextColored(ImVec4(1, 0, 0, 1), std::format("fps {:d}", final_fps).c_str());
                 if (human_with_skeleton.import_animation) {
                     ImGui::SliderFloat("speed", &human_with_skeleton.speed, 0.0f, 2.0f);
                     ImGui::SliderFloat("scale", &human_with_skeleton.scale, 0.0f, human_with_skeleton_config_scale * 2.0f);
@@ -170,7 +194,7 @@ int main()
                         ImGui::Text("Disable bone weight visualize");
                     ImGui::SliderInt("bone", &human_with_skeleton.show_bone_weight_id, -1, human_with_skeleton.bones.size() - 1);
                     ImGui::Checkbox("show bone gizmo", &show_bone_gizmo);
-                    
+
                     if (show_bone_gizmo) {
                         ImGui::SliderFloat("gizmo size", &gizmo_model.scale, 0.0f, 5.0f);
                         ImGui::ColorPicker4("gizmo color", reinterpret_cast<float*>(&bone_gizmo_color));
@@ -184,9 +208,9 @@ int main()
             glfwGetFramebufferSize(render::window::window, &render::window::SCR_WIDTH, &render::window::SCR_HEIGHT);
             projection_matrix = glm::perspectiveFov(glm::radians(60.0f), float(render::window::SCR_WIDTH), float(render::window::SCR_HEIGHT), 0.1f, 10.0f);
             glViewport(0, 0, render::window::SCR_WIDTH, render::window::SCR_HEIGHT);
- 
+
             display();
-            
+
         } while (render::window::swapAndPollInput());
     };
 
